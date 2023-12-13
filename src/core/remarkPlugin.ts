f3d772efe7440f35937974aa1580c2c242cce96a
binyellow <571704908@qq.com>
2023-12-13 21:57:29 +0800

    fix: fix number being converted to color

--- src/core/remarkPlugin.ts
+++ src/core/remarkPlugin.ts
@@ -3,12 +3,16 @@ import { unistUtilVisit } from 'dumi';
 
 export const VALID_COLOR_CHUNK = '$valid-color-chunk-inline_code';
 
+export const isNumber = (str: string) => {
+  return !isNaN(parseFloat(str)) && isFinite(Number(str));
+};
+
 function remarkPlugin() {
   return (tree: any) => {
     unistUtilVisit.visit(tree, 'inlineCode', (node, index, parent) => {
       // isValid color
       const color = new TinyColor(node.value);
-      if (color.isValid) {
+      if (color.isValid && !isNumber(node.value)) {
         parent!.children.splice(index!, 1, {
           type: 'html',
           value: `<code ${VALID_COLOR_CHUNK}=${color.toHex8String()}>${node.value}</code>`,
