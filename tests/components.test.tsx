783f983dd642febc1232405551910c86b8076d62
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-01-22 19:35:31 +0800

    feat: export Color

--- tests/components.test.tsx
+++ tests/components.test.tsx
@@ -1,9 +1,11 @@
 import { describe, expect, it } from 'vitest';
+import Color from '../src/core/Color';
 
 describe('export', () => {
   it('should work', async () => {
     const all: any = await import('dumi-plugin-color-chunk/component');
 
     expect(Object.keys(all)).toMatchSnapshot();
+    expect(all.Color).equal(Color);
   });
 });
