01e31e3262b8b97b2d76be112ef6485046c69846
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-01-23 18:56:00 +0800

    chore: update

--- cypress/e2e/image-diff.cy.ts
+++ cypress/e2e/image-diff.cy.ts
@@ -2,6 +2,8 @@ describe('website', () => {
   it('example', () => {
     cy.visit('/example/source');
 
+    cy.get('.markdown').invoke('css', 'font-family', 'unset');
+
     // 隐藏头部，规避头部遮盖内容
     cy.get('.dumi-default-header').hideElement();
 
