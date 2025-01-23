c59a57e6f81e8390bebbb96587a00ed9911faf5a
𝑾𝒖𝒙𝒉 <wxh1220@gmail.com>
2025-01-23 19:36:22 +0800

    chore: close ci

--- cypress/e2e/image-diff.cy.ts
+++ cypress/e2e/image-diff.cy.ts
@@ -1,19 +1,19 @@
 describe('website', () => {
   it('example', () => {
-    // 不需要样式，只需要内容视觉对比
-    cy.intercept(
-      {
-        url: '*.css',
-        method: 'GET',
-      },
-      {
-        statusCode: 200,
-        body: `* { font-family: "sans-serif" !important; }`,
-        headers: {
-          'content-type': 'text/css',
-        },
-      },
-    );
+    // // 不需要样式，只需要内容视觉对比
+    // cy.intercept(
+    //   {
+    //     url: '*.css',
+    //     method: 'GET',
+    //   },
+    //   {
+    //     statusCode: 200,
+    //     body: `* { font-family: "sans-serif" !important; }`,
+    //     headers: {
+    //       'content-type': 'text/css',
+    //     },
+    //   },
+    // );
 
     cy.visit('/example/source');
 
